ee41082a11ef11b4d414822d57ac98acb4963488
Manish Kumar <agilsyssoftware@gmail.com>
2024-10-28 09:54:22 +0530

    #Error Response Model Added

---
+++ ResponseModel/ErrorMessageWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MediCaresAPI.ResponseModel
+{
+    public class ErrorMessageWrapper
+    {
+        [NotMapped]
+        public bool? HasUpdated { get; set; }
+        [NotMapped]
+        public string? ErrorMessage { get; set; }
+    }
+}
